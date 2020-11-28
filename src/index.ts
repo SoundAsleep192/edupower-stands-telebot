792ed169b5978b540216d8d19f7c9baea6a85a37
Nabi Isakhanov <nabi.isakhanov@dataart.com>
2020-11-28 18:02:17 +0300

    Get rid of the abomination called sequelize

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,33 @@
-import { Telegraf } from 'telegraf';
-
-import { Sequelize } from 'sequelize';
+import { Telegraf, Markup } from 'telegraf';
 
 import { checkBotTokenProvided } from './utils/checkBotTokenProvided';
 import {config} from 'dotenv';
 config();
 
+// Token check
 const botToken = process.env.BOT_TOKEN;
 
 if (!checkBotTokenProvided(botToken)) throw new Error('A bot token was not provided!');
 
-const sequelize = new Sequelize('stand_telebot_development', 'postgres', '1234', {
-  host: 'localhost',
-  dialect: 'postgres'
-});
-sequelize.authenticate()
-  .then(() => console.log('Database connection has been established successfully.'))
-  .catch((error: Error) => console.error('Unable to connect to the database:', error));
-
-
+// Bot setup
 const bot = new Telegraf(botToken);
-bot.start((ctx) => ctx.reply('Welcome! Write /need_stand to get a random stand number'));
+
+bot.start((ctx) => ctx.reply('Hello! Please, introduce yourself'));
 bot.help((ctx) => ctx.reply('Write /need_stand to get a random stand number'));
+
 bot.command('need_stand', (ctx) => {
   const randomNumber = Math.round(Math.random() * (106 - 1) + 1);
   return ctx.reply(`You can take stand №${randomNumber}`);
 });
+
+bot.command('take', ({ reply }) => {
+  return  reply('Which stand you wanna take?', Markup
+  .keyboard(['31', '32', '/take_33'])
+  .oneTime()
+  .resize()
+  .extra()
+  );
+});
+
 bot.launch();
 console.log('Telegram bot is running! 🚀🚀🚀');
